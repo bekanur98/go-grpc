48d70318561c8b2798eeb7757b626a04fd0e8cc6
Beknur <48290597+bekanur98@users.noreply.github.com>
2025-02-14 21:46:39 +0600

    anonym func

---
+++ 6/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+  func(){
+    fmt.Println("anonym func")
+  } ()
+
+  inc := increment()
+  fmt.Println(inc())
+  fmt.Println(inc())
+  fmt.Println(inc())
+}
+
+func increment() func() int {
+  count := 0
+  return func() int {
+    return count++
+  }
+}
