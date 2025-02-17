86a77b2254bc6f0a00f628a3c2496658ef994879
Beknur Baltabaev <bekanur98@gmail.com>
2025-02-18 00:47:07 +0600

    less 7: Using ref

--- 7/main.go
+++ 7/main.go
@@ -12,9 +12,15 @@ func main() {
 	fmt.Println(msg)
 	updateMsg(&msg)
 	fmt.Println(msg)
+	updateMsg2(msg)
+	fmt.Println(msg)
 }
 
 // update msg by ref
 func updateMsg(msg *string) {
 	*msg = "Hello, World! Updated"
+}
+
+func updateMsg2(msg string) {
+	msg = "New hello world"
 }
